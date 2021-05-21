e91899c2b063a5c1ee7ce7191eddd7ae94ff3e61
ycabrer <43866176+ycabrer@users.noreply.github.com>
2021-05-21 09:47:50 -0600

    Update html.go

--- ext/html/html.go
+++ ext/html/html.go
@@ -9,7 +9,7 @@ import (
 	"net/http"
 	"strings"
 
-	"github.com/elazarl/goproxy"
+	"github.com/ycabrer/goproxy"
 	"github.com/rogpeppe/go-charset/charset"
 	_ "github.com/rogpeppe/go-charset/data"
 )
